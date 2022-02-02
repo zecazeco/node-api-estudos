a5e2bcb934c795dd82589edf9bb9387639e38e63
zecazeco <9203689+zecazeco@users.noreply.github.com>
2022-02-02 10:32:31 -0300

    adicionado informaçao de dia das partidas

--- src/services/matches.ts
+++ src/services/matches.ts
@@ -3,37 +3,43 @@ const matches = [
     id: 0,
     competition: 0,
     home: 3,
-    away: 1
+    away: 1,
+    date: '22/04/2022'
   },
   {
     id: 1,
     competition: 0,
     home: 1,
-    away: 2
+    away: 2,
+    date: '22/04/2022'
   },
   {
     id: 2,
     competition: 1,
     home: 0,
-    away: 2
+    away: 2,
+    date: '02/05/2022'
   },
   {
     id: 3,
     competition: 1,
     home: 0,
-    away: 1
+    away: 1,
+    date: '14/05/2022'
   },
   {
     id: 4,
     competition: 1,
     home: 2,
-    away: 3
+    away: 3,
+    date: '09/06/2022'
   },  
   {
     id: 5,
     competition: 2,
     home: 0,
-    away: 3
+    away: 3,
+    date: '29/07/2022'
   },  
 ];
 
